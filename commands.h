d939e752451cb89cf178197729564a593780f9e0
RoniSedakah <ronisedakah@gmail.com>
2023-01-19 17:05:50 +0200

    command 1 comment

--- commands.h
+++ commands.h
@@ -10,6 +10,7 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <fstream>
 
 
 using namespace std;
@@ -63,8 +64,9 @@ class Command{
 protected:
     DeafultIO* dio;
     string description;
+
 public:
-    Command(DeafultIO* dio, string des): dio(dio), description(des){}
+    Command(DeafultIO* dio): dio(dio){}
     virtual void execute()=0;
     virtual ~Command(){}
     virtual void print(){
@@ -72,6 +74,90 @@ public:
     }
 };
 
+/**
+ * first command.
+ */
+class UploadFiles : public Command {
+public:
+
+    UploadFiles(DeafultIO* dio) : Command(dio){
+        this->description = "1. upload an unclassified csv data files\n";
+    }
+
+    /**
+     * destructor.
+     */
+    virtual ~UploadFiles(){}
+
+    /**
+     * validate client input.
+     * @param input - string that the costumer entered.
+     * @return - -1 if the string is invalid, 0 o.w.
+     */
+    int checkInput(string input) {
+        string str2 = ".csv";
+        if(!(strstr(input.c_str(), str2.c_str()))) {
+            return -1;
+        }
+        return 0;
+    }
+
+
+    /**
+     * execute command1.
+     */
+    virtual void execute() {
+        string trainStr = "Please upload your local train CSV file.\n";
+        string testStr = "Please upload your local test CSV file.\n";
+        string complete = "Upload complete.\n";
+        string invalid = "invalid input\n";
+        //write trainStr to the client
+        dio->write(trainStr);
+        string input1;
+        int check;
+        input1 = dio->read();
+        check = checkInput(input1);
+        if(check != 0) {
+            dio->write(invalid);
+            return;
+        } else {
+            //upload to struct the file name
+            dio->write(complete);
+        }
+        dio->write(testStr);
+        string input2;
+        input2 = dio->read();
+        check = checkInput(input1);
+        if(check != 0) {
+            dio->write(invalid);
+            return;
+        } else {
+            //upload to struct the file name
+            dio->write(complete);
+        }
+    }
+
+};
+
+class ClassifyData : Command {
+public:
+
+    ClassifyData(DeafultIO* dio) : Command(dio) {
+        this->description = "3. classify data\n";
+    }
+
+    /**
+     * destructor.
+     */
+    virtual ~ClassifyData(){};
+
+    virtual void execute() {
+
+    }
+
+
+
+};
 
 #endif //MULTCLIENTREP_COMMANDS_H
 
